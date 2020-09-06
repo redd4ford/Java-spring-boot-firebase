a5df25f75be7d1bd6c66d014024a4743d0e11529
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-06 19:09:06 +0300

    Moved all the data access logic to the repository

--- src/main/java/com/redd4ford/firebase_test/service/UserService.java
+++ src/main/java/com/redd4ford/firebase_test/service/UserService.java
@@ -1,15 +1,12 @@
 package com.redd4ford.firebase_test.service;
 
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.*;
-import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.database.annotations.NotNull;
 import com.redd4ford.firebase_test.dto.UserDto;
 import com.redd4ford.firebase_test.mapper.UserMapper;
 import com.redd4ford.firebase_test.model.User;
+import com.redd4ford.firebase_test.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -18,55 +15,35 @@ import static java.util.stream.Collectors.toList;
 @Service
 public class UserService {
 
-  private final Firestore database = FirestoreClient.getFirestore();
   private final UserMapper userMapper;
+
+  private final UserRepository userRepository = new UserRepository("users");
 
   public UserService(UserMapper userMapper) {
     this.userMapper = userMapper;
   }
 
-  public UserDto save(@NotNull UserDto userDto) {
-    database
-        .collection("users")
-        .document(userDto.getEmail())
-        .set(userMapper.mapDtoToUser(userDto));
-
-    return userDto;
-
-  }
-
-  public List<UserDto> getAll() throws ExecutionException, InterruptedException {
-    List<User> userList = new ArrayList<>();
-    CollectionReference users = database.collection("users");
-
-    ApiFuture<QuerySnapshot> querySnapshot = users.get();
-    for (DocumentSnapshot doc : querySnapshot.get().getDocuments()) {
-      User user = doc.toObject(User.class);
-      userList.add(user);
-    }
-
-    return userList
+  public List<UserDto> getAll()
+      throws ExecutionException, InterruptedException {
+    return userRepository.findAll()
         .stream()
         .map(userMapper::mapUserToDto)
         .collect(toList());
   }
 
-  public UserDto getByEmail(String email) throws ExecutionException, InterruptedException {
-    DocumentReference docRef = database.collection("users").document(email);
-    // asynchronously retrieve the document
-    ApiFuture<DocumentSnapshot> future = docRef.get();
-    // block on response
-    DocumentSnapshot document = future.get();
+  public UserDto getByEmail(String email)
+      throws ExecutionException, InterruptedException {
+    User user = userRepository.findById(email);
+    return userMapper.mapUserToDto(user);
+  }
 
-    assert document.exists() : "No user found";
-    // convert document to POJO
-    User user = document.toObject(User.class);
-
+  public UserDto save(@NotNull UserDto userDto) {
+    User user = userRepository.save(userMapper.mapDtoToUser(userDto), userDto.getEmail());
     return userMapper.mapUserToDto(user);
   }
 
   public void delete(String email) {
-    database.collection("users").document(email).delete();
+    userRepository.deleteById(email);
   }
 
 }
