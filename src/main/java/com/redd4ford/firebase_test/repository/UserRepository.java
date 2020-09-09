ea33462f2c63495cf43a381e469ae6c0b9342d05
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-09 18:45:02 +0300

    Added findByUsername() and findByEmail methods

--- src/main/java/com/redd4ford/firebase_test/repository/UserRepository.java
+++ src/main/java/com/redd4ford/firebase_test/repository/UserRepository.java
@@ -3,10 +3,33 @@ package com.redd4ford.firebase_test.repository;
 import com.redd4ford.firebase_test.api.repository.FirebaseRepository;
 import com.redd4ford.firebase_test.model.User;
 
-public class UserRepository extends FirebaseRepository<User, String> {
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class UserRepository extends FirebaseRepository<User, Integer> {
 
   public UserRepository(String collectionName) {
     super(collectionName);
+  }
+
+  public User findByUsername(String username) throws ExecutionException, InterruptedException {
+    List<User> users = findAll();
+    for (User user: users) {
+      if (user.getUsername().equals(username)) {
+        return user;
+      }
+    }
+    return null;
+  }
+
+  public User findByEmail(String email) throws ExecutionException, InterruptedException {
+    List<User> users = findAll();
+    for (User user: users) {
+      if (user.getEmail().equals(email)) {
+        return user;
+      }
+    }
+    return null;
   }
 
 }
