ad23c0a96c238609b46ef1cf7b2d57e605dd0422
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-11 18:21:29 +0300

    Made Services extend AbstractService

--- src/main/java/com/redd4ford/firebase_test/service/PostService.java
+++ src/main/java/com/redd4ford/firebase_test/service/PostService.java
@@ -1,10 +1,13 @@
 package com.redd4ford.firebase_test.service;
 
-import com.google.firebase.database.annotations.NotNull;
+import com.redd4ford.firebase_test.api.repository.FirebaseRepository;
 import com.redd4ford.firebase_test.dto.PostDto;
+import com.redd4ford.firebase_test.mapper.AbstractMapper;
 import com.redd4ford.firebase_test.mapper.PostMapper;
+import com.redd4ford.firebase_test.model.Community;
 import com.redd4ford.firebase_test.model.Post;
 import com.redd4ford.firebase_test.model.User;
+import com.redd4ford.firebase_test.repository.CommunityRepository;
 import com.redd4ford.firebase_test.repository.PostRepository;
 import com.redd4ford.firebase_test.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,54 +16,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import static java.util.stream.Collectors.toList;
-
 @Service
-public class PostService {
+public class PostService extends AbstractService<Post, PostDto> {
 
   private final PostMapper postMapper;
-
   private final PostRepository postRepository = new PostRepository("posts");
   private final UserRepository userRepository = new UserRepository("users");
+  private final CommunityRepository communityRepository = new CommunityRepository("communities");
 
   public PostService(PostMapper postMapper) {
     this.postMapper = postMapper;
   }
 
-  public List<PostDto> getAll()
+  @Override
+  protected FirebaseRepository<Post, Integer> getRepository() {
+    return postRepository;
+  }
+
+  @Override
+  protected AbstractMapper<Post, PostDto> getMapper() {
+    return postMapper;
+  }
+
+  // posts - user
+  public void establishPostsUser(PostDto postDto)
       throws ExecutionException, InterruptedException {
-    return postRepository.findAll()
-        .stream()
-        .map(postMapper::mapPostToDto)
-        .collect(toList());
-  }
-
-  public PostDto getById(Integer id)
-      throws ExecutionException, InterruptedException {
-    Post post = postRepository.findById(id);
-    return postMapper.mapPostToDto(post);
-  }
-
-  public PostDto save(@NotNull PostDto postDto) throws ExecutionException, InterruptedException {
-    Post post = postRepository.save(postMapper.mapDtoToPost(postDto), postDto.getId());
-    return postMapper.mapPostToDto(post);
-  }
-
-  public void delete(Integer id) {
-    postRepository.deleteById(id);
-  }
-
-  public void setId(PostDto postDto) throws ExecutionException, InterruptedException {
-    Integer i = postRepository.countAll() + 1;
-    while (getById(i) != null) {
-      i++;
-    }
-    postDto.setId(i);
-  }
-
-  public void establishManyToOne(PostDto postDto)
-      throws ExecutionException, InterruptedException {
-    Post post = postMapper.mapDtoToPost(postDto);
+    Post post = postMapper.mapDtoToObject(postDto);
     User user = userRepository.findById(post.getUserId());
     List<Post> postList = new ArrayList<>();
     if (user.getPosts() != null) {
@@ -70,6 +51,21 @@ public class PostService {
     user.setPosts(postList);
 
     userRepository.save(user, user.getId());
+  }
+
+  // posts - community
+  public void establishPostsCommunity(PostDto postDto)
+      throws ExecutionException, InterruptedException {
+    Post post = postMapper.mapDtoToObject(postDto);
+    Community community = communityRepository.findById(post.getCommunityId());
+    List<Post> postList = new ArrayList<>();
+    if (community.getPosts() != null) {
+      postList = community.getPosts();
+    }
+    postList.add(post);
+    community.setPosts(postList);
+
+    communityRepository.save(community, community.getId());
   }
 
 }
--- src/main/java/com/redd4ford/firebase_test/service/UserService.java
+++ src/main/java/com/redd4ford/firebase_test/service/UserService.java
@@ -1,65 +1,43 @@
 package com.redd4ford.firebase_test.service;
 
-import com.google.firebase.database.annotations.NotNull;
+import com.redd4ford.firebase_test.api.repository.FirebaseRepository;
 import com.redd4ford.firebase_test.dto.UserDto;
+import com.redd4ford.firebase_test.mapper.AbstractMapper;
 import com.redd4ford.firebase_test.mapper.UserMapper;
 import com.redd4ford.firebase_test.model.User;
 import com.redd4ford.firebase_test.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import static java.util.stream.Collectors.toList;
-
 @Service
-public class UserService {
+public class UserService extends AbstractService<User, UserDto> {
 
   private final UserMapper userMapper;
-
   private final UserRepository userRepository = new UserRepository("users");
 
   public UserService(UserMapper userMapper) {
     this.userMapper = userMapper;
   }
 
-  public List<UserDto> getAll() throws ExecutionException, InterruptedException {
-    return userRepository.findAll()
-        .stream()
-        .map(userMapper::mapUserToDto)
-        .collect(toList());
+  @Override
+  protected FirebaseRepository<User, Integer> getRepository() {
+    return userRepository;
   }
 
-  public UserDto getById(Integer id) throws ExecutionException, InterruptedException {
-    User user = userRepository.findById(id);
-    return userMapper.mapUserToDto(user);
+  @Override
+  protected AbstractMapper<User, UserDto> getMapper() {
+    return userMapper;
   }
 
-  public UserDto save(@NotNull UserDto userDto) {
-    User user = userRepository.save(userMapper.mapDtoToUser(userDto), userDto.getId());
-    return userMapper.mapUserToDto(user);
-  }
-
-  public void delete(Integer id) {
-    userRepository.deleteById(id);
-  }
-
-  public boolean existsByEmail(String email)
+  public boolean isExistByEmail(String email)
       throws ExecutionException, InterruptedException {
     return userRepository.findByEmail(email) != null;
   }
 
-  public boolean existsByUsername(String username)
+  public boolean isExistByUsername(String username)
       throws ExecutionException, InterruptedException {
     return userRepository.findByUsername(username) != null;
-  }
-
-  public void setId(UserDto userDto) throws ExecutionException, InterruptedException {
-    Integer i = userRepository.countAll() + 1;
-    while (getById(i) != null) {
-      i++;
-    }
-    userDto.setId(i);
   }
 
 }
