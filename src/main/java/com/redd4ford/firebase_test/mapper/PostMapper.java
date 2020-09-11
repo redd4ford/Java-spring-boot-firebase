2a88837765503df577f426ab734bca5ab51ba1f5
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-09-11 18:11:35 +0300

    Made Mappers extend AbstractMapper

--- src/main/java/com/redd4ford/firebase_test/mapper/PostMapper.java
+++ src/main/java/com/redd4ford/firebase_test/mapper/PostMapper.java
@@ -2,20 +2,22 @@ package com.redd4ford.firebase_test.mapper;
 
 import com.redd4ford.firebase_test.dto.PostDto;
 import com.redd4ford.firebase_test.model.Post;
+import com.redd4ford.firebase_test.repository.CommunityRepository;
 import com.redd4ford.firebase_test.repository.UserRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutionException;
 
 @Component
-public class PostMapper {
+public class PostMapper extends AbstractMapper<Post, PostDto> {
 
   private final UserRepository userRepository = new UserRepository("users");
+  private final CommunityRepository communityRepository = new CommunityRepository("communities");
 
   public PostMapper() {
   }
 
-  public PostDto mapPostToDto(Post post) {
+  public PostDto mapObjectToDto(Post post) {
     if (post == null) {
       return null;
     }
@@ -23,17 +25,14 @@ public class PostMapper {
     PostDto.PostDtoBuilder postDto = PostDto.builder();
 
     postDto.id(post.getId());
-    try {
-      postDto.authorUsername(userRepository.findById(post.getUserId()).getUsername());
-    } catch (Exception e) {
-      System.err.println(e);
-    }
+    postDto.authorUsername(getAuthorUsername(post));
+    postDto.communityName(getCommunityName(post));
     postDto.text(post.getText());
 
     return postDto.build();
   }
 
-  public Post mapDtoToPost(PostDto postDto) throws ExecutionException, InterruptedException {
+  public Post mapDtoToObject(PostDto postDto) throws ExecutionException, InterruptedException {
     if (postDto == null) {
       return null;
     }
@@ -41,10 +40,36 @@ public class PostMapper {
     Post.PostBuilder post = Post.builder();
 
     post.id(postDto.getId());
-    post.userId(userRepository.findByUsername(postDto.getAuthorUsername()).getId());
+    post.userId(getUserId(postDto));
+    post.communityId(getCommunityId(postDto));
     post.text(postDto.getText());
 
     return post.build();
+  }
+
+  private String getAuthorUsername(Post post) {
+    try {
+      return userRepository.findById(post.getUserId()).getUsername();
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+    return null;
+  }
+
+  private String getCommunityName(Post post) {
+    try {
+      return communityRepository.findById(post.getCommunityId()).getCommunityName();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  private Integer getUserId(PostDto postDto) throws ExecutionException, InterruptedException {
+    return userRepository.findByUsername(postDto.getAuthorUsername()).getId();
+  }
+
+  private Integer getCommunityId(PostDto postDto) throws ExecutionException, InterruptedException {
+    return communityRepository.findByCommunityName(postDto.getCommunityName()).getId();
   }
 
 }
--- src/main/java/com/redd4ford/firebase_test/mapper/UserMapper.java
+++ src/main/java/com/redd4ford/firebase_test/mapper/UserMapper.java
@@ -2,15 +2,21 @@ package com.redd4ford.firebase_test.mapper;
 
 import com.redd4ford.firebase_test.dto.UserDto;
 import com.redd4ford.firebase_test.model.User;
+import com.redd4ford.firebase_test.repository.CommunityRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class UserMapper {
+public class UserMapper extends AbstractMapper<User, UserDto> {
+
+  private final CommunityRepository communityRepository = new CommunityRepository("communities");
 
   public UserMapper() {
   }
 
-  public UserDto mapUserToDto(User user) {
+  public UserDto mapObjectToDto(User user) {
     if (user == null) {
       return null;
     }
@@ -23,16 +29,13 @@ public class UserMapper {
     userDto.password(user.getPassword());
     userDto.location(user.getLocation());
     userDto.posts(user.getPosts());
-    if (user.getPosts() != null) {
-      userDto.postCounter(user.getPosts().size());
-    } else {
-      userDto.postCounter(0);
-    }
+    userDto.postCounter(getPostCounter(user));
+    userDto.communityNames(getCommunityNames(user));
 
     return userDto.build();
   }
 
-  public User mapDtoToUser(UserDto userDto) {
+  public User mapDtoToObject(UserDto userDto) {
     if (userDto == null) {
       return null;
     }
@@ -44,8 +47,47 @@ public class UserMapper {
     user.email(userDto.getEmail());
     user.location(userDto.getLocation());
     user.password(userDto.getPassword());
+    user.communityIds(getCommunityIds(userDto));
 
     return user.build();
+  }
+
+  private List<String> getCommunityNames(User user) {
+    if (user.getCommunityIds() == null) {
+      return null;
+    }
+    List<String> communityNamesList = new ArrayList<>();
+    for (Integer id : user.getCommunityIds()) {
+      try {
+        communityNamesList.add(communityRepository.findById(id).getCommunityName());
+      } catch (Exception e) {
+        return null;
+      }
+    }
+    return communityNamesList;
+  }
+
+  private List<Integer> getCommunityIds(UserDto userDto) {
+    if (userDto.getCommunityNames() == null) {
+      return null;
+    }
+    List<Integer> communityIdsList = new ArrayList<>();
+    for (String name : userDto.getCommunityNames()) {
+      try {
+        communityIdsList.add(communityRepository.findByCommunityName(name).getId());
+      } catch (Exception e) {
+        return null;
+      }
+    }
+    return communityIdsList;
+  }
+
+  private Integer getPostCounter(User user) {
+    if (user.getPosts() != null) {
+      return user.getPosts().size();
+    } else {
+      return 0;
+    }
   }
 
 }
